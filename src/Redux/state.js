b71028790177dc93b44493ea437def0148efcab6
nikala <mikeladzenikala@gmail.com>
2021-02-19 21:49:12 +0400

    store..

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,75 +1,74 @@
+let store = {
+   _state: {
+      profilePage: {
+         postData: [
+         { id: 1, message: "Hi, how are you?", likeCount: "13" },
+         { id: 2, message: "It's my first post", likeCount: "18" },
+         { id: 3, message: "Hello From America", likeCount: "55" },
+         ],
+         newPostText: "What's happening?",
+      },
 
-let rerenderEntireTree = () => {
-   console.log('state changed');
-}
+      messagesPage: {
+         messagesData: [
+         { id: 1, message: "Hi" },
+         { id: 2, message: "How are you?" },
+         { id: 3, message: "Wazzaapp" },
+         ],
+         newMessage: "Good work",
 
-let state = {
+         dialogsData: [
+         { id: 1, name: "David" },
+         { id: 2, name: "Nika" },
+         { id: 3, name: "Kety" },
+         { id: 4, name: "Jone" },
+         { id: 5, name: "Mery" },
+         { id: 6, name: "James" },
+         { id: 7, name: "Mia" },
+         ],
+      },
 
-   profilePage: {
-      postData: [
-         { id: 1, message: 'Hi, how are you?', likeCount: "13" },
-         { id: 2, message: 'It\'s my first post', likeCount: "18" },
-         { id: 3, message: 'Hello From America', likeCount: "55" }
-
-      ],
-      newPostText: 'What\'s happening?',
+      sidebar: {},
    },
-   
-   messagesPage: {
-      messagesData: [
-         { id: 1, message: 'Hi' },
-         { id: 2, message: 'How are you?' },
-         { id: 3, message: 'Wazzaapp' },
-      ],
-      newMessage: 'Good work',
-
-      dialogsData: [
-         { id: 1, name: 'David' },
-         { id: 2, name: 'Nika' },
-         { id: 3, name: 'Kety' },
-         { id: 4, name: 'Jone' },
-         { id: 5, name: 'Mery' },
-         { id: 6, name: 'James' },
-         { id: 7, name: 'Mia' },
-      ],
+   getState() {
+      return this._state;
+   },
+   _callSubscriber() {
+      console.log("state changed");
+   },
+   addPost() {
+      debugger;
+      let newPost = {
+         id: 4,
+         message: this._state.profilePage.newPostText,
+         likeCount: 1,
+      };
+      this._state.profilePage.postData.push(newPost);
+      this._state.profilePage.newPostText = "";
+      this._callSubscriber(this._state);
+   },
+   // ADDing post and update post
+   updateNewPostText(newText) {
+      this._state.profilePage.newPostText = newText;
+      this._callSubscriber(this._state);
+   },
+   subscribe(observer) {
+      this._callSubscriber = observer;
    },
 
-   sidebar: {}
+   // Sending message.
+   sendMessage() {
+      let newMessage = {
+         id: 4,
+         message: this._state.messagesPage.newMessage,
+      };
+      this._state.messagesPage.messagesData.push(newMessage);
+      this._state.messagesPage.newMessage = "";
+      this._callSubscriber(this._state);
+   },
+};
 
-}
-// ADDing post and update post
-export const addPost = () => {
-   let newPost = {
-      id: 4,
-      message: state.profilePage.newPostText,
-      likeCount: 1,
-   };
-   state.profilePage.postData.push(newPost);
-   state.profilePage.newPostText = '';
-   rerenderEntireTree(state);
-}
+export default store;
+window.store = store;
 
-export const updateNewPostText = (newText) => {
-   state.profilePage.newPostText = newText;
-   rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-} 
-
-// Sending message.
-export let sendMessage = () => {
-   let newMessage = {
-      id: 4,
-      message: state.messagesPage.newMessage,
-   }
-   state.messagesPage.messagesData.push(newMessage);
-   state.messagesPage.newMessage = '';
-   rerenderEntireTree(state);
-}
-
-
-
-
-export default state;
+// Store - OOP
--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, { subscribe } from './Redux/state';
+import store from './Redux/state';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updateNewPostText, sendMessage } from './Redux/state';
 
 
- let rerenderEntireTree = (state) => {
+  let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} sendMessage={sendMessage} addPost={addPost} updateNewPostText={updateNewPostText} />
+      <App state={store.getState()} sendMessage={store.sendMessage} addPost={store.addPost} updateNewPostText={store.updateNewPostText} />
     </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
